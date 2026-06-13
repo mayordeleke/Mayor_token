56cca42bc9ad84ecab1f76e16e2f3536976a54e0
Mayordeleke <mayordeleke@gmail.com>
2026-06-13 12:31:25 +0000

    updated Mayor_Token.sol

--- src/Mayor_Token.sol
+++ src/Mayor_Token.sol
@@ -3,10 +3,34 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, ERC20Burnable {
-            constructor() ERC20("Mayor Token", "MAY") {
-                        _mint(msg.sender, 1000000 * 10 ** decimals());
-            }
+contract Mayor_Token is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
+        constructor()
+                ERC20("Mayowa Token", "MAY")
+                        Ownable(msg.sender)
+                            {
+                                        _mint(msg.sender, 1000000 * 10 ** decimals());
+                            }
+
+                                function pause() public onlyOwner {
+                                            _pause();
+                                }
+
+                                    function unpause() public onlyOwner {
+                                                _unpause();
+                                    }
+
+                                        function _update(
+                                                    address from,
+                                                            address to,
+                                                                    uint256 value
+                                        ) internal override(ERC20, ERC20Pausable) {
+                                                    super._update(from, to, value);
+                                        }
 }
+                                        
+                                        
+                                
             
