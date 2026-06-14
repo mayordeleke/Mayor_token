66e11a4565193f9b2c0c25b2ce386daaf25b8fe3
Mayordeleke <mayordeleke@gmail.com>
2026-06-14 11:33:04 +0000

    updated the src file

--- src/Mayor_Token.sol
+++ src/Mayor_Token.sol
@@ -6,30 +6,40 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Mayor_Token is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
-        constructor()
-                ERC20("Mayor_Token Token", "MAY")
-                        Ownable(msg.sender)
-                            {
-                                        _mint(msg.sender, 1000000 * 10 ** decimals());
-                            }
-
-                                function pause() public onlyOwner {
-                                            _pause();
+contract MyToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
+            constructor(
+                        string memory name_,
+                                string memory symbol_,
+                                        uint256 initialSupply
+            )
+                    ERC20(name_, symbol_)
+                            Ownable(msg.sender)
+                                {
+                                                _mint(msg.sender, initialSupply * 10 ** decimals());
                                 }
 
-                                    function unpause() public onlyOwner {
-                                                _unpause();
+                                    function pause() external onlyOwner {
+                                                _pause();
                                     }
 
-                                        function _update(
-                                                    address from,
-                                                            address to,
-                                                                    uint256 value
-                                        ) internal override(ERC20, ERC20Pausable) {
-                                                    super._update(from, to, value);
+                                        function unpause() external onlyOwner {
+                                                        _unpause();
                                         }
+
+                                            function mint(address to, uint256 amount) external onlyOwner {
+                                                        _mint(to, amount * 10 ** decimals());
+                                            }
+
+                                                function _update(
+                                                                address from,
+                                                                        address to,
+                                                                                uint256 value
+                                                ) internal override(ERC20, ERC20Pausable) {
+                                                                super._update(from, to, value);
+                                                }
 }
+                                                
+
                                         
                                         
                                 
